a3daaba3f5ee137aac6e9022276bcd1ec2b5b277
cj2555 <68339127+cj2555@users.noreply.github.com>
2021-04-17 19:42:29 +0600

    first commit

---
+++ src/components/incomeExpences.js
@@ -0,0 +1,29 @@
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
+
+export const IncomeExpences = () => {
+  const { transactions } = useContext(GlobalContext);
+  const amount = transactions.map(transaction => transaction.amount);
+  const income = amount
+    .filter(item => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+  
+  const expense= (amount
+    .filter(item => item < 0)
+    .reduce((acc, item) => (acc += item), 0))*-1
+  
+    return (
+        <div>
+             <div className="inc-exp-container">
+        <div>
+          <h4>Income</h4>
+            <p id="money-plus" className="money plus">{income}</p>
+        </div>
+        <div>
+          <h4>Expense</h4>
+          <p id="money-minus" className="money minus">{expense}</p>
+        </div>
+      </div>
+        </div>
+    )
+}
